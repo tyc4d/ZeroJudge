b5a49701d0c5015bf663d7d6822e98a5bc7754e4
ed0419 <genden80@gmail.com>
2020-10-22 21:02:28 +0800

    ***有問題

--- a058.py
+++ a058.py
@@ -11,3 +11,21 @@ for i in range(n,0,-1):
   if i%3==2:
     kc+=1
 print(ka,kb,kc)
+
+
+'''
+可通過測資的程式碼
+'''
+
+n=int(input())
+ka=0
+kb=0
+kc=0
+for i in range(0,n):
+  if i%3==0:
+    ka+=1
+  if i%3==1:
+    kb+=1
+  if i%3==2:
+    kc+=1
+print(ka,kb,kc)
